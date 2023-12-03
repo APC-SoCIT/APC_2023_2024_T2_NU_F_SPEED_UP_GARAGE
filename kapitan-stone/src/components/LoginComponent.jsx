df3eef1010ba7238c5bf1eb1b2aba62ef9d19419
esurenajt1 <esurenajt1@student.apc.edu.ph>
2023-12-03 18:55:41 +0800

    Changed the Login UI and adding login into their class names

--- kapitan-stone/src/components/LoginComponent.jsx
+++ kapitan-stone/src/components/LoginComponent.jsx
@@ -10,16 +10,17 @@ const LoginComponent = () => {
       <div className="login-div">
         <div className="login-inner-div">
           <img src={logoImage} alt="logo" className="logo" />
-          <p className="titleText">Speed Up Garage Inventory<br />and POS System</p>
+          <p className="title-text">Speed Up Garage Inventory<br />and POS System</p>
+            <div>
+              <label htmlFor="uname" className="login-label">Username</label>
+              <input type="text" placeholder="Username" name="uname" className="login-input" required />
+              <label htmlFor="psw" className="login-label">Password</label>
+              <input type="password" placeholder="Password" name="psw" className="login-password-input" required />
+
+              <button className="login-button">Login</button>
+            </div>
           <div>
-            <label htmlFor="uname" className="label">Username</label>
-            <input type="text" placeholder="Enter Username" name="uname" className="input" required />
-            <label htmlFor="psw" className="label">Password</label>
-            <input type="password" placeholder="Enter Password" name="psw" className="password input" required />
-            <button className="login-button">Login</button>
-          </div>
-          <div>
-            <span className="error" id="error">This is for error handling</span>
+            <span className="login-error" id="error">This is for error handling</span>
           </div>
         </div>
       </div>
--- kapitan-stone/src/index.js
+++ kapitan-stone/src/index.js
@@ -4,10 +4,12 @@ import './index.css';
 import App from './App';
 import Login from './Login';
 import LoginComponent from './components/LoginComponent';
+import Header from './Header';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    {/*<App />*/}
     <LoginComponent />
   </React.StrictMode>
 );
