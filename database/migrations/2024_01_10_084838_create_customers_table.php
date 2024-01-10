c7b5dce76609e43dffbc2513856d13f9c04a7f77
esurenajt1 <esurenajt1@student.apc.edu.ph>
2024-01-10 19:06:51 +0800

    CRUD Functionality added to Customer.php

---
+++ database/migrations/2024_01_10_084838_create_customers_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCustomersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('customers', function (Blueprint $table) {
+            $table->id();
+            $table->string('customer_name');
+            $table->string('phone');
+            $table->text('address');
+            // You can add more columns as needed
+            $table->timestamps(); // Adds created_at and updated_at columns
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('customers');
+    }
+}
+
