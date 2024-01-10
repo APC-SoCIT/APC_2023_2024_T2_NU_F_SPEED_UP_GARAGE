4716aa73c9b073fb64c04257acb2d32360fea0f8
esurenajt1 <esurenajt1@student.apc.edu.ph>
2024-01-10 08:13:27 +0800

    working database for inventory

---
+++ kapitan-stone/resources/views/dbconn.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>DB Connection</title>
+</head>
+<body>
+    <div>
+        <?php
+            use Illuminate\Support\Facades\DB;
+            if(DB::connection()->getPdo()){
+                echo "Successfully connected to the database named " . DB::connection()->getDatabaseName();
+
+            }
+        ?>
+    </div>
+</body>
+</html>
